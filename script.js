5d0e5a3cb322ff513917026fafa694bb16b97ab6
arbereshahoxha <ah67620@ubt-uni.net>
2023-11-30 23:30:22 +0100

    form validation almost done , homepage changes ne links edhe ne genres qeto links horrible po doken nfund ama do ti afroj kismeeet

--- script.js
+++ script.js
@@ -1,29 +1,59 @@
 // Validimi i formave - sign up
 
-function validateForm(){
-    var name = document.getElementById('name').value;
-    var name = document.getElementById('email').value;
-    var name = document.getElementById('password').value;
-    var name = document.getElementById('passwordConfirm').value;
+// function validateForm(){
+//     var name = document.getElementById('name').value;
+//     var email = document.getElementById('email').value;
+//     var password = document.getElementById('password').value;
+//     var confirmPassword = document.getElementById('passwordConfirm').value;
     
-    var nameRegex = /^[a-zA-Z\s]$/;
-    if(!nameRegex.test(name)){
-        aler('Please enter a valid name');
-        return false;
-    }
-    var emailRegex = /^$/;
-    if(!emailRegex.test(email)){
-        aler('Please enter a valid email');
-        return false;
-    }
-    if(password.length < 8){
-        alert('Password must be at least 8 characters ');
+//     var nameRegex = /^[a-zA-Z\s]$/;
+//     if(!nameRegex.test(name)){
+//         alert('Please enter a valid name');
+//         return false;
+//     }
+//     var emailRegex = /^[^\s@]+@[^\s@]+[^\s@]+$/;
+//     if(!emailRegex.test(email)){
+//         alert('Please enter a valid email');
+//         return false;
+//     }
+//     if(password.length < 8){
+//         alert('Password must be at least 8 characters ');
+//         return false;
+//     }
+//     if(password !== confirmPassword){
+//         alert('Passwords do not match')
+//         return false;
+//     }
+//     return true;
+// }
+function validateForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var confirmPassword = document.getElementById('passwordConfirm').value;
+
+    var nameRegex = /^[a-zA-Z\s]*$/;
+    if (!nameRegex.test(name)) {
+        alert('Please enter a valid name');
         return false;
     }
 
-    if(password !== confirmPassword){
-        alert('Passwords do not match')
+    var emailRegex = /^\S+@\S+\.\S+$/;
+    if (!emailRegex.test(email)) {
+        alert('Please enter a valid email');
         return false;
     }
-    
+
+    if (password.length < 8) {
+        alert('Password must be at least 8 characters');
+        return false;
+    }
+
+    if (password !== confirmPassword) {
+        alert('Passwords do not match');
+        return false;
+    }
+
+    alert('Signup successful!');
+    return true;
 }
