f6f77f610bff0393606d4f68607052626a7a8ff8
arbereshahoxha <ah67620@ubt-uni.net>
2023-12-09 20:20:09 +0100

    finished form validation on sign up page

--- script.js
+++ script.js
@@ -1,63 +1,36 @@
 // Validimi i formave - sign up
 
-// function validateForm(){
-//     var name = document.getElementById('name').value;
-//     var email = document.getElementById('email').value;
-//     var password = document.getElementById('password').value;
-//     var confirmPassword = document.getElementById('passwordConfirm').value;
-    
-//     var nameRegex = /^[a-zA-Z\s]$/;
-//     if(!nameRegex.test(name)){
-//         alert('Please enter a valid name');
-//         return false;
-//     }
-//     var emailRegex = /^[^\s@]+@[^\s@]+[^\s@]+$/;
-//     if(!emailRegex.test(email)){
-//         alert('Please enter a valid email');
-//         return false;
-//     }
-//     if(password.length < 8){
-//         alert('Password must be at least 8 characters ');
-//         return false;
-//     }
-//     if(password !== confirmPassword){
-//         alert('Passwords do not match')
-//         return false;
-//     }
-//     return true;
-// }
 function validateForm() {
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
     var confirmPassword = document.getElementById('passwordConfirm').value;
 
+    var isValid = true;
     var nameRegex = /^[a-zA-Z\s]*$/;
     if (!nameRegex.test(name)) {
         alert('Please enter a valid name');
-        return false;
+        isValid = false;
+    }
+    var emailRegex = /^\S+@\S+\.\S*$/;
+    if (email.trim() !== '' && !emailRegex.test(email)) {
+        alert("Please enter a valid email");
+        isValid = false;
+    }
+    if (password.trim() !== '' && password.length < 8) {
+        alert("Password must be at least 8 characters");
+        isValid = false;
+    }
+    if (confirmPassword !== password) {
+        alert("Passwords do not match");
+        isValid = false;
+    }
+    if (isValid && !alert('Signup successful!')) {
+        document.getElementById("yourFormId").submit();
     }
 
-    var emailRegex = /^\S+@\S+\.\S+$/;
-    if (!emailRegex.test(email)) {
-        alert('Please enter a valid email');
-        return false;
-    }
-
-    if (password.length < 8) {
-        alert('Password must be at least 8 characters');
-        return false;
-    }
-
-    if (password !== confirmPassword) {
-        alert('Passwords do not match');
-        return false;
-    }
-
-    alert('Signup successful!');
-    return true;
+    return isValid;
 }
-
 //audio player
 function playSong(song) {
     let player = document.getElementById("player");
