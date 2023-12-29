84db1cf4c0f16f1e2ef07ea0f6cf8232679e50a9
ArbisonKrasniqi <ak64951@ubt-uni.net>
2023-12-29 18:00:47 +0100

    Made custom media player using css and javascript. Currently to play the songs, they have to be added to the songs folder and then the mediaPlayer.js file has to be edited to include the name of the song in the songs array the buttons play the song by using a song pointer variable that points to the array index. currently there are only 6 songs in the array. The css for the media player is in the homepage.css file at the end

--- homepage.php
+++ homepage.php
@@ -159,11 +159,30 @@
         
     </main>
     <div id="home-mediaPlayer">
-                <audio controls id="home-player">
-                    <source src="" type="audio/mp3">
-                    Your browser does not support audio.
-                </audio>
+    <audio src="" type ="audio/mp3" preload="metadata" loop></audio>
+            <div id="home-songInformation">
+                asd
+            </div>
+            <div id="home-centerPlayer">
+                <div id="home-playButtons">
+                    <button class="home-playerButton" id="home-previous-button"><img src="img/previus-button.png" alt="previous" id="home-previous-icon"></button>
+                    <button class="home-playerButton" id="home-play-button"><img src="img/play-button-white.png" alt="play/pause" id="home-play-icon"></button>
+                    <button class="home-playerButton" id="home-next-button"><img src="img/next-button.png" alt="next" id="home-next-icon"></button>
+                </div>
+                <div id="home-lowerPlayer">
+                    <span id="home-current-time" class="home-time">0:00</span>
+                    <input type="range" class="home-longSlider" id="home-seek-slider" max="100" value="0">
+                    <span id="home-duration" class="home-time">0:00</span>
+                </div>
+            </div>
+            
+            <div id="home-volumeControls">
+                <output id="home-volume-output">100</output>
+                <button class="home-playerButton" id="home-mute-button"><img src="img/volume-button.png" alt="play/pause" id="home-mute-icon"></button>
+                <input type="range" class="home-shortSlider" id="home-volume-slider" max="100" value="100">
+            </div>
     </div>
     <script src="script.js"></script>
+    <script src="mediaPlayer.js"></script>
 </body>
 </html>
---
+++ mediaPlayer.js
@@ -0,0 +1,171 @@
+const audioPlayerContainer = document.getElementById("home-container");
+const playButton = document.getElementById("home-play-button");
+const playIconContainer = document.getElementById("home-play-icon");
+const muteButton = document.getElementById("home-mute-button");
+const muteIconContainer = document.getElementById("home-mute-icon");
+const seekSlider = document.getElementById("home-seek-slider");
+const volumeSlider = document.getElementById("home-volume-slider");
+const outputContainer = document.getElementById("home-volume-output");
+
+const previousButton = document.getElementById("home-previous-button");
+const nextButton = document.getElementById("home-next-button");
+
+
+
+//Gets the audio element from html file
+const audio = document.querySelector("audio");
+const durationContainer = document.getElementById("home-duration");
+
+//By default the music is paused and unmuted
+let playState = "play";
+let muteState = "unmuted";
+
+
+let songPointer = 0;
+let songs = [
+            "songs/DhurataDoraElvanaGjataGajde.mp3",
+            "songs/ElaiJoti.mp3",
+            "songs/ElaiUnMeTy.mp3",
+            "songs/YllLimaniDafinaZeqiriKaje.mp3",
+            "songs/YllLimaniMeJetu.mp3",
+            "songs/YllLimaniPerKonJeZbukuru.mp3"
+            ];
+audio.src = songs[0];
+
+
+//Whenever the button is clicked, play previous music
+previousButton.addEventListener('click', () => {
+    previousSong();
+});
+
+nextButton.addEventListener('click', () => {
+    nextSong();
+});
+
+//Whenever the button is clicked, pause or play the music
+playButton.addEventListener('click', () => {
+    if(playState == "play") {
+        audio.play();
+        playIconContainer.src = "icons/pause-button-white.png";
+        playState = "pause";
+    } else {
+        audio.pause();
+        playIconContainer.src = "icons/play-button-white.png";
+        playState = "play";
+    }
+});
+
+//Whenever the button is clicked, mute or unmute the music
+muteButton.addEventListener('click', () => {
+    if(muteState == "unmuted") {
+        muteIconContainer.src = "icons/mute-button.png";
+        audio.muted = true;
+        muteState = "muted";
+    } else {
+        muteIconContainer.src = "icons/volume-button.png";
+        audio.muted = false;
+        muteState = "unmuted";
+    }
+});
+
+
+
+
+//Converts seconds to 00:00 format
+const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+    const seconds = Math.floor(secs % 60);
+    const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${minutes}:${returnedSeconds}`;
+}
+
+//Changes duration relative to audio length
+const displayDuration = () => {
+    durationContainer.textContent = calculateTime(audio.duration);
+}
+
+
+//The maximum value of the slider is the length of the song
+const setSliderMax = () => {
+    seekSlider.max = Math.floor(audio.duration);
+}
+
+//If the song is loaded, it displays the duration
+//It also sets the maximum value of the slider to the length of the song
+const loadSong = () => {
+    if (audio.readyState > 0) {
+        displayDuration();
+        setSliderMax();
+    } else {
+        audio.addEventListener('loadedmetadata', () => {
+            displayDuration();
+            setSliderMax();
+        });
+    }
+}
+
+//Plays previous song
+const previousSong = () => {
+    if (songPointer == 0) {
+        songPointer = songs.length - 1;
+        audio.src = songs[songPointer];
+        loadSong();
+        audio.play();
+        playState = "pause";
+        playIconContainer.src = "icons/pause-button-white.png";
+    } else {
+        songPointer -= 1;
+        audio.src = songs[songPointer];
+        loadSong();
+        audio.play();
+        playState = "pause";
+        playIconContainer.src = "icons/pause-button-white.png";
+    }
+}
+
+//Plays next song
+const nextSong = () => {
+    if (songPointer == songs.length - 1) {
+        songPointer = 0;
+        audio.src = songs[songPointer];
+        loadSong();
+        audio.play();
+        playState = "pause";
+        playIconContainer.src = "icons/pause-button-white.png";
+    } else {
+        songPointer += 1;
+        audio.src = songs[songPointer];
+        loadSong();
+        audio.play();
+        playState = "pause";
+        playIconContainer.src = "icons/pause-button-white.png";
+    }
+}
+//This function executes on startup
+loadSong();
+
+
+//Update current time text
+const currentTimeContainer = document.getElementById("home-current-time");
+seekSlider.addEventListener('input', () => {
+    currentTimeContainer.textContent = calculateTime(seekSlider.value);
+})
+
+//User changes audio time
+seekSlider.addEventListener('change', () => {
+    audio.currentTime = seekSlider.value;
+});
+
+//Audio updates slider every second
+audio.addEventListener('timeupdate', () => {
+    seekSlider.value = Math.floor(audio.currentTime);
+    currentTimeContainer.textContent = calculateTime(seekSlider.value);
+});
+
+//User changes volume
+volumeSlider.addEventListener('input', (e) => {
+    const value =e.target.value;
+
+    outputContainer.textContent = value;
+    audio.volume = value/100;
+});
