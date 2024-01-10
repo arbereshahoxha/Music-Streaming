052f6058028016f7659895db9f0c8cba79552f2f
arbereshahoxha <ah67620@ubt-uni.net>
2024-01-10 16:09:29 +0100

    added connection w the database , started working on the blog

---
+++ db.php
@@ -0,0 +1,22 @@
+<?php
+
+class DatabaseConnection{
+    private $server = "localhost";
+    private $username = "root";
+    private $password = "";
+    private $database = "musicstreaming";
+
+    public function startConnection(){
+        if(!$conn = mysqli_connect($this->server,$this->username,$password,$database)){
+            echo "Error connecting to the database";
+            return null;
+        }else{
+            echo "Succsesful connection to the databasee";
+            return $conn;
+        }
+    }
+}
+$db = new DatabaseConnection();
+$db -> startConnection();
+
+?>
