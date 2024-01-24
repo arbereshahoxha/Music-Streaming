1a71bdfe86aa990fe175e280205519d01060282d
ArbisonKrasniqi <ak64951@ubt-uni.net>
2024-01-24 21:57:04 +0100

    Uploading songs is now Object oriented

--- dynamicDiv/song.php
+++ dynamicDiv/song.php
@@ -1,15 +1,21 @@
 <?php
+include("../db.php");
     class Song {
+        private $id;
         private $songName;
         private $songAuthors;
         private $songImage;
         private $songMedia;
 
-        public function __construct($songName, $songAuthors, $songImage, $songMedia) {
+        public function __construct($id, $songName, $songAuthors, $songImage, $songMedia) {
+            $this->id = $id;
             $this->songName = $songName;
             $this->songAuthors = $songAuthors;
             $this->songImage = $songImage;
             $this->songMedia = $songMedia;
+        }
+        public function getID() {
+            return $this->id;
         }
 
         public function getSongName() {
@@ -54,6 +60,49 @@
             echo "</div>";
         }
 
-    }
+        public function displayYourSong() {
+            echo "<div class='songs-kanget'". "onclick='playRandomSong()'>";
+            echo "<img src='fotot/$this->songImage' alt='song-image'>";
+            echo "<h2>$this->songName</h2>";
+            echo "<p>$this->songAuthors</p>";
+            echo "</div>";
+        }
 
+        public function displayYourSongs($conn) {
+            //TODO
+        }
+
+        public function ekziston($conn) {
+            $sql = "SELECT * FROM song WHERE songName='$this->songName'";
+            $result = mysqli_query($conn, $sql);
+            $count_song = mysqli_num_rows($result);
+
+            $sql = "SELECT * FROM song WHERE songAuthors='$this->songAuthors'";
+            $result = mysqli_query($conn, $sql);
+            $count_artist = mysqli_num_rows($result);
+
+            if ($count_song == 0 && $count_artist == 0) {
+                return false; //Nuk ekziston
+            } else {
+                return true; //Ekziston ne databaze
+            }
+        }
+
+        public function addToDatabase($conn) {
+            if(!$this->ekziston($conn)) {
+                $sql = "INSERT INTO song(songName, songAuthors, songImage) VALUES('$this->songName','$this->songAuthors', '$this->songImage')";
+                $result = mysqli_query($conn, $sql);
+
+                if ($result) {
+                    echo '<script>alert("Song added succesfully");</script>';
+                    header("Location: homepage.php");
+                } else {
+                    echo '<script>alert("There has been a server error!");</script>';
+                }
+
+            } else {
+                echo '<script>alert("This song already exists!");</script>';
+            }
+        }   
+    }
 ?>
--- upload-songs.php
+++ upload-songs.php
@@ -1,37 +1,18 @@
 <?php
-include("db.php");
 include("dynamicDiv/song.php");
 
 if (isset($_POST['submit'])) {
-    $coverPhoto = $_POST['songName'];
-    $emri = $_POST['songAuthors'];
-    $description = $_POST['songImage'];
-    $readMore = $_POST['songMedia'];
-    $role = 0;
+    $song = new Song(
+        0, //Random ID
+        $_POST['songName'],
+        $_POST['songAuthors'],
+        $_POST['songImage'],
+        $_POST['songMedia'],
+    );
 
-    // Check if a user with the same name or email already exists
-    $sql = "SELECT * FROM song WHERE songName='$songName'";
-    $result = mysqli_query($conn, $sql);
-    $count_song = mysqli_num_rows($result);
-
-    $sql = "SELECT * FROM song WHERE songAuthors='$songAuthors'";
-    $result = mysqli_query($conn, $sql);
-    $count_artist = mysqli_num_rows($result);
-
-    if ($count_song == 0 || $count_artist == 0) {
-        $sql = "INSERT INTO song(songName, songAuthors, songImage, songMedia) VALUES('$songName','$songAuthors', '$songImage', '$songMedia')";
-        $result = mysqli_query($conn, $sql);
-
-        if ($result) {
-            echo '<script>alert("Song added succesfully");</script>';
-            header("Location: homepage.php");
-        }
-    } elseif ($count_user > 0) {
-        echo '<script>alert("This song already exists");</script>';
-    }else {
-        echo '<script>alert("There has been a server error");</script>';
-    }
+    $song->addToDatabase($conn);
 }
+
 ?>
 
 <h1>Upload Songs</h1>
